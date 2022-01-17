2a652dc369d338927f6032ae993c816738388739
Shakir Ansari <shakir.ansari@live.com>
2022-01-17 15:20:20 +0530

    Create footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="text-end border p-4 my-4">
+      <h6 class="footer">
+        Written by <em>ErShakirAnsari</em>
+        <br />
+        &copy; 2019
+      </h6>
+    </footer>
+  );
+};
+
+export default Footer;
