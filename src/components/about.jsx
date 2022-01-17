f27463e1873ac5c2a977f147d84a09cea5f4b4f0
Shakir Ansari <shakir.ansari@live.com>
2022-01-17 15:20:17 +0530

    Create about.jsx

---
+++ src/components/about.jsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div className="container mt-4">
+      <div class="text-center">
+        Rest client is a light weight but powerfull tool to test your rest apis with support of analyze and validate
+        responses.
+        <br />
+        Advance features like auto save last request makes it advacne rest client.
+      </div>
+    </div>
+  );
+};
+
+export default About;
